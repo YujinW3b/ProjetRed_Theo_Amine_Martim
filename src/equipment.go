f1b864f62bf75a0f26c686120388b54c22e6aeea
Théo Sugier <theo.sugier@ynov.com>
2026-09-18 16:58:36 +0200

    style: applique go fmt sur les fichiers du squelette

--- src/equipment.go
+++ src/equipment.go
@@ -3,4 +3,3 @@ package main
 // THEO - T17 : equiper, desequiper, recalcul des PV max.
 // La structure Equipment est dans character.go (carte T16 de Martim).
 // UNE SEULE fonction calcule les PV max, et replafonne les PV actuels.
-
