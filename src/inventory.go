1bfbcf218c4c10524e774ed92141b216c5ffdb19
Amine KOUAIDI <amine.kouaidi@ynov.com>
2026-09-19 14:30:37 +0200

    inventory

--- src/inventory.go
+++ src/inventory.go
@@ -1,6 +1,23 @@
 package main
 
-// AMINE - T07 addInventory/removeInventory, T12 limite, T18 upgradeInventorySlot.
-// Ces deux fonctions sont les SEULS points d entree pour modifier l inventaire.
-// La limite se branche dans addInventory, et nulle part ailleurs.
+import "fmt"
 
+func addInventory(c *Character, item string) bool {
+	if len(c.Inventaire) >= 10 {
+		fmt.Println("Inventaire plein ! Impossible d'ajouter :", item)
+		return false
+	}
+	c.Inventaire = append(c.Inventaire, item)
+	return true
+}
+
+func removeInventory(c *Character, item string) bool {
+	for i, it := range c.Inventaire {
+		if it == item {
+			c.Inventaire = append(c.Inventaire[:i], c.Inventaire[i+1:]...)
+			return true
+		}
+	}
+	fmt.Println("Item introuvable dans l'inventaire :", item)
+	return false
+}
