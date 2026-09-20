b6a481425908fd349495b6257e27fb5dda08e6c6
Théo Sugier <theo.sugier@ynov.com>
2026-09-20 23:08:56 +0200

    chore(main): retire le bloc de test temporaire

--- src/main.go
+++ src/main.go
@@ -1,17 +1,9 @@
 package main
 
-import "fmt"
-
 // je le declare ici et pas dans main() pour que menu.go puisse le voir aussi
 var joueur Character
 
 func main() {
-	// TEST TEMPOR
-	fmt.Println(isNameValid("Theo"))       // true
-	fmt.Println(isNameValid(""))           // false
-	fmt.Println(isNameValid("Bo3"))        // false
-	fmt.Println(isNameValid("le sergent")) // false, l'espace
-	fmt.Println(isNameValid("Théo"))       // false, le "e" accentue tient sur 2 bytes et aucun n'est entre 'a' et 'z'
 
 	joueur.initCharacter("Recrue", "Humain", 1, 100, 50, []string{})
 	menuPrincipal()
