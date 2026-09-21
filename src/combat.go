1d95389fea6a0f7040af0c95bbeacb039502f842
Théo Sugier <theo.sugier@ynov.com>
2026-09-21 21:43:48 +0200

    feat(combat): ajoute le calcul des degats du gobelin

--- src/combat.go
+++ src/combat.go
@@ -2,3 +2,10 @@ package main
 
 // THEO - T20 goblinPattern, T21 characterTurn, T22 trainingFight, M1, M2.
 // Le compteur de tours vit ici et est PASSE a goblinPattern.
+
+func goblinDamage(attack int, turn int) int {
+	if turn%3 == 0 { // tours 3, 6, 9... le gobelin cogne double
+		return attack * 2
+	}
+	return attack
+}
