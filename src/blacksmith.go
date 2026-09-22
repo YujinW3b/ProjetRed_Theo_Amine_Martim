04d76d2a5bc51ee2bc40e11f63370bba9c181466
Amine KOUAIDI <amine.kouaidi@ynov.com>
2026-09-22 13:37:03 +0200

    feat poison potion

--- src/blacksmith.go
+++ src/blacksmith.go
@@ -1,5 +1,73 @@
 package src
 
-// AMINE - T15 la Forge : recettes, controles, fabrication.
-// Ordre : materiaux -> or -> place -> puis consommer, debiter, ajouter.
-// Un echec ne consomme NI materiaux NI or.
+import "fmt"
+
+const prixForgeron = 5
+
+func recetteOK(c *Character, materiaux map[string]int) bool {
+	for item, qte := range materiaux {
+		if countItem(c.Inventaire, item) < qte {
+			return false
+		}
+	}
+	return true
+}
+
+func totalMateriaux(materiaux map[string]int) int {
+	total := 0
+	for _, qte := range materiaux {
+		total += qte
+	}
+	return total
+}
+
+func fabriquer(c *Character, nomEquipement string, materiaux map[string]int) {
+	if !recetteOK(c, materiaux) {
+		fmt.Println("Sergent : « Il te manque des matériaux, recrue. »")
+		return
+	}
+	if c.Or < prixForgeron {
+		fmt.Println("Sergent : « T'as pas assez d'or pour ça, recrue. »")
+		return
+	}
+	if len(c.Inventaire)-totalMateriaux(materiaux)+1 > c.InventaireMax {
+		fmt.Println("Sergent : « Ta besace déborde, recrue. »")
+		return
+	}
+
+	for item, qte := range materiaux {
+		for i := 0; i < qte; i++ {
+			removeInventory(c, item)
+		}
+	}
+	c.Or -= prixForgeron
+	addInventory(c, nomEquipement)
+	fmt.Printf("Sergent : « %s forgé ! Or restant : %d »\n", nomEquipement, c.Or)
+}
+
+func accessForgeron(c *Character) {
+	for {
+		fmt.Println("=== Le Forgeron ===")
+		fmt.Println("1 - Chapeau de l'aventurier (1 Plume de Corbeau + 1 Cuir de Sanglier, 5 or)")
+		fmt.Println("2 - Tunique de l'aventurier (2 Fourrures de Loup + 1 Peau de Troll, 5 or)")
+		fmt.Println("3 - Bottes de l'aventurier (1 Fourrure de Loup + 1 Cuir de Sanglier, 5 or)")
+		fmt.Println("0 - Retour au feu de camp.")
+		fmt.Println("Votre choix ?")
+
+		var choix int
+		fmt.Scan(&choix)
+
+		switch choix {
+		case 0:
+			return
+		case 1:
+			fabriquer(c, itemChapeauAventurier, map[string]int{itemPlumeDeCorbeau: 1, itemCuirDeSanglier: 1})
+		case 2:
+			fabriquer(c, itemTuniqueAventurier, map[string]int{itemFourrureDeLoup: 2, itemPeauDeTroll: 1})
+		case 3:
+			fabriquer(c, itemBottesAventurier, map[string]int{itemFourrureDeLoup: 1, itemCuirDeSanglier: 1})
+		default:
+			fmt.Println("Sergent : « Erreur dans le choix... »")
+		}
+	}
+}
--- src/inventory.go
+++ src/inventory.go
@@ -2,32 +2,44 @@ package src
 
 import "fmt"
 
-func checkInventoryLimit(c *Character) bool {
-	return len(c.Inventaire) < 10 // T18 : a remplacer par une capacite stockee dans Character
-}
+const (
+	itemPotionDeVie       = "Potion de vie"
+	itemPotionDePoison    = "Potion de poison"
+	itemLivreBouleDeFeu   = "Livre de Sort : Boule de Feu"
+	itemFourrureDeLoup    = "Fourrure de Loup"
+	itemPeauDeTroll       = "Peau de Troll"
+	itemCuirDeSanglier    = "Cuir de Sanglier"
+	itemPlumeDeCorbeau    = "Plume de Corbeau"
+	itemAugmentationInv   = "Augmentation d'inventaire"
+	itemChapeauAventurier = "Chapeau de l'aventurier"
+	itemTuniqueAventurier = "Tunique de l'aventurier"
+	itemBottesAventurier  = "Bottes de l'aventurier"
 
-func accessInventory(c *Character, item string) {
-	fmt.Println("=== Affichage de l'inventaire ===")
-	for itemIndex, itemName := range c.Inventaire {
-		fmt.Printf("\t %d - %s\n", (itemIndex + 1), itemName)
-	}
-	if len(c.Inventaire) == 0 {
-		fmt.Println("Inventaire vide ...")
-	}
-	fmt.Printf("%d/%d\n", len(c.Inventaire), 10)
-	for true {
-		fmt.Println("0 - retour au feu de champs.")
-		fmt.Println("Votre choix ?")
-		var chose int
-		fmt.Scan(&chose)
-		switch chose {
-		case 0:
-			break
-		default:
-			fmt.Println("Erreur dans le choix...")
+	spellCoupDePoing = "Coup de poing"
+	spellBouleDeFeu  = "Boule de Feu"
+)
+
+func findItemIndex(inventaire []string, nom string) int {
+	for i, item := range inventaire {
+		if item == nom {
+			return i
 		}
 	}
+	return -1
+}
 
+func countItem(inventaire []string, nom string) int {
+	count := 0
+	for _, item := range inventaire {
+		if item == nom {
+			count++
+		}
+	}
+	return count
+}
+
+func checkInventoryLimit(c *Character) bool {
+	return len(c.Inventaire) < c.InventaireMax
 }
 
 func addInventory(c *Character, item string) bool {
@@ -47,4 +59,73 @@ func removeInventory(c *Character, item string) bool {
 	}
 	c.Inventaire = append(c.Inventaire[:i], c.Inventaire[i+1:]...)
 	return true
+}
+
+func takePot(c *Character) {
+	i := findItemIndex(c.Inventaire, itemPotionDeVie)
+
+	switch {
+	case i == -1:
+		fmt.Println("Sergent : « Ta besace est VIDE, pas de potion, pas de soin ! »")
+	case c.Pv >= c.Pvmax:
+		fmt.Println("Sergent : « PV au max, on ne gâche pas une potion ! »")
+	default:
+		removeInventory(c, itemPotionDeVie)
+		c.Pv += 50
+		if c.Pv > c.Pvmax {
+			c.Pv = c.Pvmax
+		}
+		fmt.Printf("Sergent : « Potion avalée ! PV : %d/%d »\n", c.Pv, c.Pvmax)
+	}
+}
+
+func upgradeInventorySlot(c *Character) bool {
+	if c.InventoryUpgradesUsed >= 3 {
+		fmt.Println("Sergent : « Le Régisseur n'a plus de besace renforcée à te vendre, recrue. »")
+		return false
+	}
+	c.InventaireMax += 10
+	c.InventoryUpgradesUsed++
+	fmt.Printf("Sergent : « Nouvelle besace ! Capacité : %d emplacements. »\n", c.InventaireMax)
+	return true
+}
+
+func accessInventory(c *Character) {
+	for {
+		fmt.Println("=== Affichage de l'inventaire ===")
+		if len(c.Inventaire) == 0 {
+			fmt.Println("Inventaire vide ...")
+		} else {
+			for itemIndex, itemName := range c.Inventaire {
+				fmt.Printf("\t %d - %s\n", itemIndex+1, itemName)
+			}
+		}
+		fmt.Printf("%d/%d\n", len(c.Inventaire), c.InventaireMax)
+
+		fmt.Println("0 - Retour au feu de camp.")
+		fmt.Println("Votre choix ?")
+
+		var choix int
+		fmt.Scan(&choix)
+
+		if choix == 0 {
+			return
+		}
+		if choix < 1 || choix > len(c.Inventaire) {
+			fmt.Println("Sergent : « Erreur dans le choix... »")
+			continue
+		}
+
+		itemChoisi := c.Inventaire[choix-1]
+		switch itemChoisi {
+		case itemPotionDeVie:
+			takePot(c)
+		case itemLivreBouleDeFeu:
+			if spellBook(c) {
+				removeInventory(c, itemLivreBouleDeFeu)
+			}
+		default:
+			fmt.Println("Sergent : « Cet objet ne peut pas être utilisé ici. »")
+		}
+	}
 }
